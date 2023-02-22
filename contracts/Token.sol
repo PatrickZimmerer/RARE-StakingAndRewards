f5671e778e9cff606ff7d26a84af471a8d41ad53
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-22 19:40:10 +0100

    added addStaking and withdrawStaking + stakingWallet mapping

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import {ERC20Capped, ERC20} from "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import {Ownable} from "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title Basic ERC20 contract where the controller contract can mint
@@ -11,47 +11,46 @@ import {Ownable} from "../node_modules/@openzeppelin/contracts/access/Ownable.so
  * @dev the owner can withdraw the balance of the contract
  */
 contract Token is ERC20Capped, Ownable {
-    uint256 public constant MAX_SUPPLY = 10_000_000 * 10 ** 18;
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 10 ** 18;
+    uint256 public constant STAKING_AMOUNT = 10 ether; // 10 tokens
+
+    mapping(address => uint256) stakingWallet;
 
     constructor() ERC20("JustAToken", "JAT") ERC20Capped(MAX_SUPPLY) {
-        _mint(msg.sender, 1_000_000 * 10 ** uint256(decimals()));
+        ERC20._mint(msg.sender, 10_000 * 1 ether);
     }
 
+    //TODO: Add function that checks if the smart contract thats calling a specific method is the
+    //TODO: controller contract
     modifier onlyControllerContract() {
-        // require(
-        //     msg.sender = controllerContract,
-        //     "only the controller contract can do that"
-        // );
+        require(true, "false");
         _;
     }
 
     /*
-     * @title Owner mint tokens
-     * @author Patrick Zimmerer
-     * @notice the owner can mint new tokens by his likings
-     * @dev _amountInToken gets passed in by owner when calling the contract
+     * @title Staking wallet of the user who is staking Nfts gets increased by STAKING_AMOUNT tokens * the amount of nfts which are getting staked
+     * @notice this can only be called by the controllerContract
+     * @dev _nftsInStaking gets passed in by controller contract
      */
-    function ownerMintNewTokens(uint256 _amountInToken) external onlyOwner {
-        _mint(msg.sender, _amountInToken * 10 ** uint256(decimals()));
-    }
-
-    /*
-     * @title Mint tokens for staking called by controlller contract
-     * @author Patrick Zimmerer
-     * @notice the controller contract can mint new tokens for staking
-     * @dev _amountInToken gets passed in by controller contract
-     */
-    function stakingMint(
-        uint256 _amountInToken
+    function addStakingAmount(
+        uint256 _nftsInStaking
     ) external onlyControllerContract {
-        _mint(msg.sender, _amountInToken * 10 ** uint256(decimals())); // decimals get added in here
+        stakingWallet[msg.sender] += STAKING_AMOUNT * _nftsInStaking;
     }
 
     /*
-     * @title Basic withdraw function
-     * @author Patrick Zimmerer
-     * @notice every user can mint as many NFT as the maxSupply supplies
-     * @dev the owner can withdraw the balance of the contract
+     * @title Withdraws the amount of staked tokens a user has in his stakingWallet
+     * @notice the controller contract can withdraw the staking rewards
+     * @dev could be changed to only transfer a specific amount of the tokens but needs a require then
+     */
+    //TODO: currently sending it to the MintAndStake contract => tokens need to go to the staking user
+    function stakingWithdraw() external onlyControllerContract {
+        _mint(msg.sender, stakingWallet[msg.sender]);
+    }
+
+    /*
+     * @title Admin withdraw function
+     * @notice The admin can withdraw the balance of this contract
      */
     function ownerWithdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
