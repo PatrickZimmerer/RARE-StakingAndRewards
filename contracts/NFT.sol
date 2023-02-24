ccd24605bf778843d5e0a1ec5193844c6c0179a8
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-24 19:48:59 +0100

    added transfer function for NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,27 +2,48 @@
 pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract NFT is ERC721 {
+    using Strings for uint256;
     uint256 public tokenSupply = 1;
     uint256 public constant MAX_SUPPLY = 6;
     uint256 public constant PRICE = 0.0001 ether;
 
+    mapping(uint256 => address) private _owners;
+    mapping(address => mapping(address => bool)) private _operators;
+
     address immutable deployer;
 
     constructor() ERC721("SimpleNFT", "SIT") {
-        deployer = msg.sender;
+        deployer = _msgSender();
     }
 
     function mint() external payable {
         uint256 _tokenSupply = tokenSupply; // added local variable to reduce gas cost
         require(_tokenSupply < MAX_SUPPLY, "Max supply reached");
         require(msg.value == PRICE, "Not enough ETH sent");
-        _mint(msg.sender, _tokenSupply);
+        _mint(_msgSender(), _tokenSupply);
         unchecked {
             _tokenSupply++; // added unchecked block since overflow check gets handled by require MAX_SUPPLY
         }
         tokenSupply = _tokenSupply;
+        _owners[_tokenSupply] = _msgSender();
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public override {
+        require(_owners[_tokenId] != address(0), "Token does not exist");
+        require(_owners[_tokenId] == _from, "Can't transfer from non-owner");
+        require(
+            _msgSender() == _from || _operators[_from][_msgSender()],
+            "Required to be owner or operator"
+        );
+        _owners[_tokenId] = _to;
+        _transfer(_from, _to, _tokenId);
     }
 
     function _baseURI() internal pure override returns (string memory) {
